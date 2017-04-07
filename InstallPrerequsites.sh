f803ff576564eea4e46102f811a7f2bec7b99f5c
Pillager225 <ryanloringcooper@gmail.com>
2017-04-07 12:25:40 -0700

    removed unecessary folders and added an dep install script

---
+++ InstallPrerequsites.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+pip install pyserial
+
+git clone https://github.com/pozyxLabs/Pozyx-Python-library.git && cd Pozyx-Python-library
+python setup.py install
+cd ../
+
+git clone Pillager225@gitlab.com:DecaBot/JUDS.git && cd JUDS
+./autoconf.sh && JAVA_HOME=/usr/lib/jvm/java-8-openjdk ./configure
+make -j2 && mv juds-*.jar juds.jar
+cd ../
+
+git clone https://Pillager225@gitlab.com/DecaBot/DataturbineExporter.git
+cp JUDS/juds.jar DataturbineExporter
+cd DataturbineExporter
+make -j2 JAR=/usr/lib/jvm/java-8-openjdk/bin/jar 
+cd ../
+
+git clone https://github.com/KittsSeniorDesign/DifferentialDrive.git
+cp DataturbineExporter/build/DataturbineExporter.jar DifferentialDrive/
+cp JUDS/juds.jar DifferentialDrive/
+
