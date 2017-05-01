63ace15e5bf20289fff032c3bbc631e9250a5a75
Pillager225 <ryanloringcooper@gmail.com>
2017-05-01 13:34:07 -0700

    made install script better

--- InstallPrerequsites.sh
+++ InstallPrerequsites.sh
@@ -1,24 +1,14 @@
-#!/bin/bash
-
 cd ../
-
-pip install pyserial
-
-git clone https://github.com/pozyxLabs/Pozyx-Python-library.git && cd Pozyx-Python-library
+git clone https://Pillager225@gitlab.com/DecaBot/JUDS.git
+cd JUDS
+./autoconf.sh
+JAVA_HOME=/usr/lib/jvm/java-8-openjdk ./configure
+make
+cd ../
+git clone https://github.com/pozyxLabs/Pozyx-Python-library.git
+cd Pozyx-Python-library/
 python setup.py install
 cd ../
-
-git clone Pillager225@gitlab.com:DecaBot/JUDS.git && cd JUDS
-./autoconf.sh && JAVA_HOME=/usr/lib/jvm/java-8-openjdk ./configure
-make -j2 && mv juds-*.jar juds.jar
-cd ../
-
-git clone https://Pillager225@gitlab.com/DecaBot/DataturbineExporter.git
-cp JUDS/juds.jar DataturbineExporter
-cd DataturbineExporter
-make -j2 JAR=/usr/lib/jvm/java-8-openjdk/bin/jar 
-cd ../
-
-cp DataturbineExporter/build/DataturbineExporter.jar DifferentialDrive/
-cp JUDS/juds.jar DifferentialDrive/
-
+pip install pyserial
+cd DifferentialDrive
+./UpdateDataturbineExporter.sh
